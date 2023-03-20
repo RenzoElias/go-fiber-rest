76605b0400348409fda51d6a4a7a572a8afce07b
RenzoElias <renzoeliasdelacalle@gmail.com>
2023-03-20 15:26:06 -0500

    ADD endpoints

--- routes/movies.routes.go
+++ routes/movies.routes.go
@@ -28,4 +28,101 @@ func UseMoviesRoute(router fiber.Router) {
 			"movies": movies,
 		})
 	})
+
+	router.Get("/:id", func(c *fiber.Ctx) error {
+
+		id, _ := c.ParamsInt("id")
+
+		var movieFound Movie
+
+		for _, movie := range movies {
+			if movie.Id == id {
+				movieFound = *movie
+			}
+		}
+
+		return c.JSON(fiber.Map{
+			"movies": movieFound,
+		})
+	})
+
+	router.Post("/", func(c *fiber.Ctx) error {
+
+		type Request struct {
+			Title string
+			Id    int
+		}
+		var body Request
+		c.BodyParser(&body)
+		// fmt.Println(body)
+
+		// CREAR NEW ITEM
+		newMovie := Movie{
+			Title: body.Title,
+			Id:    len(movies) + 1,
+		}
+		// fmt.Println(newMovie)
+
+		// AGREGAR EL NEW ITEM
+		movies = append(movies, &newMovie)
+
+		return c.JSON(fiber.Map{
+			"movies": movies,
+		})
+	})
+
+	router.Put("/:id", func(c *fiber.Ctx) error {
+
+		// RECOGER EL PARAMETRO DEL ENDPOINT
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "there's a error",
+			})
+		}
+
+		// RECOGER EL BODY JSON
+		type Request struct {
+			Title string
+			Id    int
+		}
+		var body Request
+		c.BodyParser(&body)
+
+		// REEMPLAZAR
+		for _, movie := range movies {
+			if movie.Id == id {
+				movie.Title = body.Title
+			}
+		}
+
+		return c.JSON(fiber.Map{
+			"movies": movies,
+		})
+	})
+
+	router.Delete("/:id", func(c *fiber.Ctx) error {
+
+		// RECOGER EL PARAMETRO DEL ENDPOINT
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "there's a error",
+			})
+		}
+
+		// REEMPLAZAR
+		for idx, movie := range movies {
+			if movie.Id == id {
+				movies = append(movies[:idx], movies[idx+1:]...)
+			}
+		}
+
+		return c.JSON(fiber.Map{
+			"movies": movies,
+		})
+	})
+
 }
